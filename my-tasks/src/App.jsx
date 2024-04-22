7fbcb66ed6514a6c3f808beee1b992b6c58fe938
Barbara Viana <vianabsouza@gmail.com>
2024-04-22 15:07:18 -0300

    create 'addtasks' and 'button' components, and change extension files to jsx

--- my-tasks/src/App.jsx
+++ my-tasks/src/App.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 
 import Tasks from "./components/Tasks";
 import "./App.css";
+import AddTask from "./components/AddTask";
 
 const App = () => {
   const [tasks, setTasks] = useState([
@@ -17,9 +18,23 @@ const App = () => {
     },
   ]);
 
+  const handleTaskAddition = (taskTitle) => {
+    const newTask = [
+      ...tasks,
+      {
+        id: Math.random(10),
+        title: taskTitle,
+        completed: false,
+      },
+    ];
+
+    setTasks(newTask);
+  };
+
   return (
     <>
       <div className="container">
+        <AddTask handleTaskAddition={handleTaskAddition} />
         <Tasks tasks={tasks} />
       </div>
     </>
---
+++ my-tasks/src/components/AddTask.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+
+import "./AddTask.css";
+import Button from "./Button";
+
+const AddTask = ({ handleTaskAddition }) => {
+  const [inputData, setInputData] = useState("");
+
+  const handleInputChange = (e) => {
+    console.log(e.target.value);
+    setInputData(e.target.value);
+  };
+
+  const handleTaskClick = () => {
+    handleTaskAddition(inputData);
+  };
+
+  return (
+    <div className="add-task-container">
+      <input
+        onChange={handleInputChange}
+        value={inputData}
+        className="add-task-input"
+        type="text"
+      />
+      <div className="add-task-button-container">
+        <Button onClick={handleTaskClick}>Adicionar</Button>
+      </div>
+    </div>
+  );
+};
+
+export default AddTask;
