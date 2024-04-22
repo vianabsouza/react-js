338a2499d1edcaf2ef8e2eca66369e9ce208b2a1
Barbara Viana <vianabsouza@gmail.com>
2024-04-22 16:21:32 -0300

    implement click for completed task or not

--- my-tasks/src/App.jsx
+++ my-tasks/src/App.jsx
@@ -19,6 +19,14 @@ const App = () => {
     },
   ]);
 
+  const handleTaskClick = (taskId) => {
+    const newTasks = tasks.map((task) => {
+      if (task.id === taskId) return { ...task, completed: !task.completed };
+      return task;
+    });
+    setTasks(newTasks);
+  };
+
   const handleTaskAddition = (taskTitle) => {
     const newTask = [
       ...tasks,
@@ -36,7 +44,7 @@ const App = () => {
     <>
       <div className="container">
         <AddTask handleTaskAddition={handleTaskAddition} />
-        <Tasks tasks={tasks} />
+        <Tasks tasks={tasks} handleTaskClick={handleTaskClick} />
       </div>
     </>
   );
