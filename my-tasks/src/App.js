d20f573426d0077863079a469c81ed695bd14d72
Barbara Viana <vianabsouza@gmail.com>
2024-04-22 02:11:22 -0300

    create 'tasks' and 'task' components + add some styles

---
+++ my-tasks/src/App.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+
+import Tasks from "./components/Tasks";
+import "./App.css";
+
+const App = () => {
+  const [tasks, setTasks] = useState([
+    {
+      id: "1",
+      title: "Estudar programação",
+      completed: false,
+    },
+    {
+      id: "2",
+      title: "Ler livros",
+      completed: true,
+    },
+  ]);
+
+  return (
+    <>
+      <div className="container">
+        <Tasks tasks={tasks} />
+      </div>
+    </>
+  );
+};
+
+export default App;
